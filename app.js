09d0c452a84df9fd9bdacf0662a3925852a29bb2
shireen.koul@nuance.com <shireen.koul@nuance.com>
2020-03-18 08:45:12 -0400

    Add a simple function

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('Code starts here!!!!');
+function add(n1, n2) {
+    return n1 + n2;
+}
+var n1 = 5;
+var n2 = 2.5;
+var result = add(n1, n2);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,1 +1,9 @@
-console.log('Code starts here!!!!');
+function add(n1 : number , n2 : number){
+    return n1 + n2;
+}
+
+const n1 = 5;
+const n2 = 2.5;
+
+const result = add(n1, n2);
+console.log(result);
